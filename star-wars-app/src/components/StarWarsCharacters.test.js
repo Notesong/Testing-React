555610b097eb6e39c8677eb7ae8a77ddf8702f2e
Lee Damien <notesong.git@outlook.com>
2020-02-13 00:58:28 -0500

    Seperate tests and adjust jest.mock for seperating

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { render, fireEvent, wait, act } from '@testing-library/react'
 import { getData as mockGetData } from '../api'
+import axios from 'axios';
 import StarWarsCharacters from './StarWarsCharacters'
 
-jest.mock('../api')
+jest.mock('axios', () => {
+    return {
+        get: jest.fn(() => Promise.resolve({
+            data: {
+                results: [{
+                    name: 'Luke Skywalker',
+                    url: 'https://swapi.co/api/people/'
+                }],
+                next: "next page",
+                previous: null    
+            }
+        }))
+    }
+});
 
-test('next button and previous button working and API data being called and rendering', async () => {
-    mockGetData.mockResolvedValueOnce({
-        results: [{
-            name: 'Luke Skywalker',
-            url: 'https://swapi.co/api/people/1/'
-        }],
-        next: "next page",
-        previous: null
-    })
-
+test('next button and previous button working', async () => {
     const { getByText } = render(<StarWarsCharacters />)
     
     // set buttons to variables and click them
@@ -27,5 +32,17 @@ test('next button and previous button working and API data being called and rend
     await wait(() => expect(getByText(/luke/i)))
 
     // make sure data has been called
-    expect(mockGetData).toHaveBeenCalledTimes(1)
+    expect(axios.get).toHaveBeenCalledTimes(1)
+})
+
+test('data rendering', async () => {
+    const { getByText } = render(<StarWarsCharacters />)
+
+    // check if data has loaded
+    await wait(() => expect(getByText(/luke/i)))
+})
+
+test('API data being called', async () => {
+    // make sure data has been called
+    expect(axios.get).toHaveBeenCalledTimes(2)
 })
